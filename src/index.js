d021bb7693f5507c90b268c771262bd065a4098d
Andrew <71823699+riezong@users.noreply.github.com>
2025-05-13 14:46:48 +1000

    Convert promise to async/await

--- src/index.js
+++ src/index.js
@@ -2,31 +2,21 @@ import "./styles.css";
 
 // GIPHY
 const img = document.querySelector("img");
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=mc5mG0JWFoAUnogxO2r7DcxFqyyhm3Yu&s=cats",
-  { mode: "cors" },
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+
+async function getCats() {
+  const response = await fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=mc5mG0JWFoAUnogxO2r7DcxFqyyhm3Yu&s=cats",
+    { mode: "cors" },
+  );
+  const catData = await response.json();
+  img.src = catData.data.images.original.url;
+}
+getCats();
 
 const newCatGifBtn = document.querySelector("#newCatGifBtn");
 newCatGifBtn.addEventListener("click", function () {
   img.src = "";
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=mc5mG0JWFoAUnogxO2r7DcxFqyyhm3Yu&s=cats",
-    { mode: "cors" },
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-      GifHeading.textContent = "Random Cat GIF";
-    });
+  getCats();
 });
 
 const GifSearchBtn = document.querySelector("#GifSearchBtn");
@@ -36,20 +26,18 @@ GifSearchBtn.addEventListener("click", function () {
   img.src = "";
   let s = GifSearchInput.value;
   console.log(s);
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=mc5mG0JWFoAUnogxO2r7DcxFqyyhm3Yu&s=" +
-      s,
-    { mode: "cors" },
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-      GifHeading.textContent = "Random " + s + " GIF";
-      GifSearchInput.value = "";
-    })
-    .catch(function (err) {});
+  async function getNewGif() {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=mc5mG0JWFoAUnogxO2r7DcxFqyyhm3Yu&s=" +
+        s,
+      { mode: "cors" },
+    );
+    const newGifData = await response.json();
+    img.src = newGifData.data.images.original.url;
+    GifHeading.textContent = "Random " + s + " GIF";
+    GifSearchInput.value = "";
+  }
+  getNewGif();
 });
 
 // Visual Crossing
